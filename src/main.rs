7961e9966fcff56899e96cd58317818703efe95c
Sébastien Guimmara <sguimmara@pobox.com>
2022-01-29 11:36:06 +0100

    add clap crate and simple <hash> subcommand to the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use clap::{arg, App, AppSettings};
+
 mod core;
 
 fn main() {
-    println!("Hello, world!");
+    let app = clap::App::new("authenticator")
+        .bin_name("authenticator")
+        .setting(clap::AppSettings::SubcommandRequired)
+        .subcommand(
+            App::new("hash")
+                .about("return the SHA-256 hash of the supplied text")
+                .arg(arg!(<TEXT> "the text to"))
+                .setting(AppSettings::ArgRequiredElseHelp),
+        )
+        .get_matches();
+
+    match app.subcommand() {
+        Some(("hash", sub_matches)) => {
+            let text = sub_matches.value_of("TEXT").unwrap();
+            println!("SHA-256 of \"{}\": {}", text, core::hash(text));
+        }
+        _ => unreachable!(),
+    }
 }
